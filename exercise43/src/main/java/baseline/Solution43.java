9b4d7eafc9523ac535f6672e83683a3937c6930e
aidane1023 <90140945+aidane1023@users.noreply.github.com>
2021-10-16 14:11:51 -0400

    added copyright

--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -1,3 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 aidan earnest
+ */
+
+/*
+Create a program that generates a website skeleton with the following specifications:
+Prompt for the name of the site.
+Prompt for the author of the site.
+Ask if the user wants a folder for JavaScript files.
+Ask if the user wants a folder for CSS files.
+Generate an index.html file that contains the name of the site inside the <title> tag and the author in a <meta> tag.
+ */
 package baseline;
 
 import java.io.File;
